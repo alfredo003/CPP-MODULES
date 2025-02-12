5ceeabd2eb3a0a0617b653c4da681f446e526df9
Alfredo Manuel <59560615+alfredo003@users.noreply.github.com>
2025-02-12 16:35:13 +0000

    CPP01/ex04

--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@ class Weapon
 private:
     std::string _type;
 public:
-    Weapon(std:string type);
+    Weapon(std::string type);
 
     void setType(std::string type);
     const std::string &getType() const;
