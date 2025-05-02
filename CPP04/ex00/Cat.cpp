cd5901947a0be7300912513072c6c3e0ff56f16e
alfredo003 <alfredochivela3@gmail.com>
2025-05-02 16:43:51 +0100

    build: CPP03/ex00

---
+++ CPP04/ex00/Cat.cpp
@@ -0,0 +1,30 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+	std::cout << "Cat Default constructor called" << std::endl;
+}
+Cat::Cat::~Cat()
+{
+	std::cout << "Cat desctructor called" << std::endl;
+}
+
+Cat::Cat(Cat const &copy) : Animal(copy.type)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat &Cat::operator=(Cat const &copy)
+{
+	std::cout << "Cat copy operator assignment called" << std::endl;
+	if (this != &copy)
+	{
+		type = copy.type;
+	}
+	return (*this);
+}
+	
+void	Cat::makeSound(void) const
+{
+	std::cout << "miau miau miau" << std::endl;
+}
